1eea147a9d7a2f84c560813bf3638fe444f07e2f
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-30 19:49:45 +0600

    lab 9 - ok!

--- lab-9-col/src/ru/luxoft/cources/Main.java
+++ lab-9-col/src/ru/luxoft/cources/Main.java
@@ -1,6 +1,5 @@
 package ru.luxoft.cources;
 
-import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -8,14 +7,14 @@ public class Main {
 
     public static void main(String[] args) {
         //Карта покупателей с картой покупок покупателя
-        TreeMap<String, TreeMap<String, Integer>> customers = new TreeMap<String, TreeMap <String, Integer>>();
+        TreeMap<String, TreeMap<String, Integer>> customers = new TreeMap<>();
 
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
-        while(sc.hasNext()) {
+        while (sc.hasNext()) {
             String s = sc.nextLine();
 
-            if(s.equals("exit")) {
+            if (s.equals("exit")) {
                 break;
             }
 
@@ -25,31 +24,22 @@ public class Main {
             String goodName = parts[1];
             Integer count = Integer.parseInt(parts[2]);
 
-            if (!customers.containsKey(customerName))
-                customers.put(customerName, new TreeMap <String, Integer>());
+            customers.putIfAbsent(customerName, new TreeMap<>());
 
-            TreeMap <String, Integer> custTmp = customers.get(customerName);
+            TreeMap<String, Integer> custTmp = customers.get(customerName);
 
-            if (!custTmp.containsKey(goodName))
-                custTmp.put(goodName,0);
+            custTmp.putIfAbsent(goodName, 0);
 
             Integer oldCount = custTmp.get(goodName);
 
             custTmp.put(goodName, oldCount + count);
         }
 
-        for(Map.Entry<String, TreeMap <String, Integer>> entry : customers.entrySet()) {
-            String key = entry.getKey();
-            TreeMap <String, Integer> value = entry.getValue();
-
-            System.out.println(key + ":");
-
-            for(Map.Entry<String,Integer> good : value.entrySet()) {
-                String keyGood = good.getKey();
-                Integer valueGood = good.getValue();
-
-                System.out.println("     "+keyGood + " " + valueGood);
-            }
-        }
+        customers.forEach((k, v) -> {
+            System.out.println(k + ":");
+            v.forEach(
+                    (k1, v1) -> System.out.println("     " + k1 + " " + v1)
+            );
+        });
     }
 }
