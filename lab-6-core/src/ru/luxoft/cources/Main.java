7613ca0cb118326b5d3c5a6598c0457ef5c40988
Denis Tsyganov <dtsyganov@luxoft.com>
2021-04-30 19:13:57 +0600

    lab 6 - ok!

--- lab-6-core/src/ru/luxoft/cources/Main.java
+++ lab-6-core/src/ru/luxoft/cources/Main.java
@@ -1,35 +1,36 @@
 package ru.luxoft.cources;
 
 //import java.lang.reflect.Array;
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+
+    private static final int PRODUCTS_LEN = 5;
+
     public static void main(String[] args) {
         // 3
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         // 5
-        String[] masOfProducts = new String[5];
-        Integer [] masOfCosts = new Integer [5];
-        Integer[] masOfCounts = new Integer[5];
+        String[] masOfProducts = new String[PRODUCTS_LEN];
+        Integer[] masOfCosts = new Integer[PRODUCTS_LEN];
+        Integer[] masOfCounts = new Integer[PRODUCTS_LEN];
 
         int count = 0;
-        while(sc.hasNext() && count < 5) {
+        while (sc.hasNext() && count < PRODUCTS_LEN) {
             String s = sc.nextLine();
-            if("exit".equals(s)) {
+            if ("exit".equals(s)) {
                 break;
             }
             count++;
-        // 4
+            // 4
             String[] parts = s.split(" ");
             // 15
-            if(parts.length != 3){
+            if (parts.length != 3) {
                 System.out.println("Wrong number of arguments! Retry!");
                 continue;
             }
-
-            //String productName = parts[0];
-            //Integer productCost = Integer.parseInt(parts[1]);
-            //Integer productCount = Integer.parseInt(parts[2]);
 
             String productName = parts[0];
             Integer productCost;
@@ -37,7 +38,7 @@ public class Main {
             try {
                 productCost = Integer.parseInt(parts[1]);
                 productCount = Integer.parseInt(parts[2]);
-            } catch (NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 System.out.println(ex.getMessage());
                 System.out.println("Retry!");
                 continue;
@@ -46,17 +47,17 @@ public class Main {
 
             // 6
             boolean productAlreadyExists = false;
-            for (int i = 0; i < 5; i++) {
-                if(productName.equals(masOfProducts[i])){
+            for (int i = 0; i < PRODUCTS_LEN; i++) {
+                if (productName.equals(masOfProducts[i])) {
                     masOfCosts[i] = productCost;
                     masOfCounts[i] += productCount;
                     productAlreadyExists = true;
                 }
             }
-        // 7
-            if(!productAlreadyExists) {
-                for (int i = 0; i < 5; i++) {
-                    if(masOfProducts[i] == null){
+            // 7
+            if (!productAlreadyExists) {
+                for (int i = 0; i < PRODUCTS_LEN; i++) {
+                    if (masOfProducts[i] == null) {
                         masOfProducts[i] = productName;
                         masOfCosts[i] = productCost;
                         masOfCounts[i] = productCount;
@@ -67,40 +68,31 @@ public class Main {
 
         }
         // 8
-        String[] sortedProducts = new String[5];
-        System.arraycopy(masOfProducts, 0, sortedProducts, 0, 5);
+        String[] sortedProducts = new String[PRODUCTS_LEN];
+        System.arraycopy(masOfProducts, 0, sortedProducts, 0, PRODUCTS_LEN);
         // 9
-        Arrays.sort(sortedProducts, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                int res = String.CASE_INSENSITIVE_ORDER.compare(o1, o2);
-                if (res == 0) {
-                    res = o1.compareTo(o2);
-                }
-                return res;
-            }
-        });
+        Arrays.sort(sortedProducts, String.CASE_INSENSITIVE_ORDER.thenComparing(String::compareTo));
         // 10
         System.out.println(Arrays.toString(sortedProducts));
         // 11
         long sum = 0;
-        for (int i = 0; i < 5; i++) {
-            sum += masOfCosts[i] * masOfCounts[i];
+        for (int i = 0; i < PRODUCTS_LEN; i++) {
+            sum += (long) masOfCosts[i] * masOfCounts[i];
         }
         System.out.println(sum);
 
         // 12
         int indexOfMostPopularProduct = 0;
-        for (int i = 0; i < 5; i++) {
-            if(masOfCounts[i] > masOfCounts[indexOfMostPopularProduct]){
+        for (int i = 0; i < PRODUCTS_LEN; i++) {
+            if (masOfCounts[i] > masOfCounts[indexOfMostPopularProduct]) {
                 indexOfMostPopularProduct = i;
             }
         }
         // 13 - 14
         for (int j = 0; j < 3; j++) {
             // int indexOfMostPopularProduct = 0;
-            for (int i = 0; i < 5; i++) {
-                if(masOfCounts[i] > masOfCounts[indexOfMostPopularProduct]){
+            for (int i = 0; i < PRODUCTS_LEN; i++) {
+                if (masOfCounts[i] > masOfCounts[indexOfMostPopularProduct]) {
                     indexOfMostPopularProduct = i;
                 }
             }
@@ -109,8 +101,6 @@ public class Main {
             masOfCounts[indexOfMostPopularProduct] = 0;
             masOfCosts[indexOfMostPopularProduct] = 0;
         }
-
-
 
 
     }
